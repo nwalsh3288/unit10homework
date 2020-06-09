bfd22cb9b85793425f9e81d3e78fb377ffc37510
Nicole Walsh <nmwalsh92@gmail.com>
2020-06-08 22:15:59 -0700

    draft 1

---
+++ Develop/test.js
@@ -0,0 +1,5 @@
+const Engineer = require ("./lib/Engineer")
+
+const engineer= new Engineer("Nicole", "1234", "nicole@gmail.com", "nwalsh3288");
+console.log(engineer.getID())
+console.log(engineer.getGithub())
